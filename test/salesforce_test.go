5b38f2ae8d73d4ace4af3b1b495896dc4c508967
Richard Weston <github@riweston.io>
2021-11-15 13:31:37 +0000

    wip: Staging Cobra framework

--- test/salesforce_test.go
+++ test/salesforce_test.go
@@ -1,4 +1,4 @@
-package gopsa_test
+package gopsa
 
 import (
 	"testing"
@@ -6,10 +6,19 @@ import (
 
 func TestSetConfig(t *testing.T) {
 	const (
+		endpoint string = "https://cloudreach.my.salesforce.com/"
+		username string = "user@cloudreach.com"
+		password string = "password"
+		token    string = "token"
 	)
 	t.Run("SetConfig", func(t *testing.T) {
 		got := setConfig(endpoint, username, password, token)
-		want := ""
+		want := AppConfig{
+			endpoint: "https://cloudreach.my.salesforce.com/",
+			username: "user@cloudreach.com",
+			password: "password",
+			token:    "token",
+		}
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
