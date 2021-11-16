b30b6852b9bafa5506ccb2e2b6c826f744433ae2
Richard Weston <github@riweston.io>
2021-11-16 11:04:55 +0000

    wip: workbench file with PoC functions

--- cmd/.workbench.go
+++ cmd/.workbench.go
@@ -7,27 +7,7 @@ import (
 	"github.com/simpleforce/simpleforce"
 )
 
-const (
-)
-
-type AppConfig struct {
-	endpoint string
-	username string
-	password string
-	token    string
-}
-
-func setConfig(endpoint string, username string, password string, token string) (appConfig AppConfig) {
-	appConfig = AppConfig{
-		endpoint: endpoint,
-		username: username,
-		password: password,
-		token:    token,
-	}
-	return
-}
-
-func getUserId(appConfig AppConfig) (userId string) {
+func GetUserId(appConfig AppConfig) (userId string) {
 	/* 	endpoint := os.Getenv("endpoint")
 	   	username := os.Getenv("username")
 	   	password := os.Getenv("password")
@@ -73,13 +53,28 @@ func getAssignmentsAll(appConfig AppConfig, userId string) {
 		return
 	}
 	filter := []string{
-		"select Id",
-		"Name",
-		"pse__Project__c",
-		"pse__Project__r.Name",
-		"pse__Project__r.pse__Is_Billable__c from pse__Assignment__c where pse__Resource__c = '" + userId + "' and Open_up_Assignment_for_Time_entry__c = false and pse__Closed_for_Time_Entry__c = false",
+		"select",
+		"Id,",
+		"Name,",
+		"pse__Project__c,",
+		"pse__Project__r.Name,",
+		"pse__Project__r.pse__Is_Billable__c",
+		"from",
+		"pse__Assignment__c",
+		"where",
+		"pse__Resource__c",
+		"=",
+		fmt.Sprint("'%s'", userId),
+		"and",
+		"Open_up_Assignment_for_Time_entry__c",
+		"=",
+		"false",
+		"and",
+		"pse__Closed_for_Time_Entry__c",
+		"=",
+		"false",
 	}
-	query := strings.Join(filter, ", ")
+	query := strings.Join(filter, " ")
 	result, err := client.Query(query)
 	if err != nil {
 		// handle the error
@@ -91,8 +86,8 @@ func getAssignmentsAll(appConfig AppConfig, userId string) {
 }
 
 func main() {
-	config := setConfig(endpoint, username, password, token)
-	storeConfig(config)
-	userId := getUserId(config)
+	config := SetConfig(endpoint, username, password, token)
+	StoreConfig(config)
+	userId := GetUserId(config)
 	fmt.Print(userId)
 }
